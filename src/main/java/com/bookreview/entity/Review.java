7864376fb355d350632143402cb33e0e3be4b755
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-01-11 17:38:42 +0300

    add review entity

---
+++ src/main/java/com/bookreview/entity/Review.java
@@ -0,0 +1,44 @@
+package com.bookreview.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @Column(name = "book_id")
+    private Book book;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}

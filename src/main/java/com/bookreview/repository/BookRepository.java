7d49e5503123202d89ed975180fb84d136f932e0
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-01-12 01:21:33 +0300

    add extra methods to repos

--- src/main/java/com/bookreview/repository/BookRepository.java
+++ src/main/java/com/bookreview/repository/BookRepository.java
@@ -2,6 +2,12 @@ package com.bookreview.repository;
 
 import com.bookreview.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Book b WHERE b.title = :title")
+    boolean existsByTitle(@Param("title") String title);
+
 }
--- src/main/java/com/bookreview/repository/UserRepository.java
+++ src/main/java/com/bookreview/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.bookreview.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
 }

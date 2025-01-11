2edfcc6c7298caf9e47f3ddb3cde844b6fae6cb5
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-01-11 23:41:26 +0300

    add book controller

---
+++ src/main/java/com/bookreview/controller/BookController.java
@@ -0,0 +1,47 @@
+package com.bookreview.controller;
+
+
+import com.bookreview.entity.Book;
+import com.bookreview.service.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private BookServiceImpl bookService;
+
+    @Autowired
+    public BookController(BookServiceImpl bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteBook(@PathVariable Long id) {
+        return bookService.deleteBook(id);
+    }
+}

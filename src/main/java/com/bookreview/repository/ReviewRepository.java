20defbcded95e3599169713368cde58336d99c1b
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-01-11 23:51:48 +0300

    add query method to reviewrepo

--- src/main/java/com/bookreview/repository/ReviewRepository.java
+++ src/main/java/com/bookreview/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.bookreview.repository;
 
+import com.bookreview.entity.Book;
 import com.bookreview.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByBook(Book book);
+
 }

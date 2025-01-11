fc8215a2277c733e68a15a166222d3b9ae0e196c
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-01-12 00:28:58 +0300

    fix review repository

--- src/main/java/com/bookreview/repository/ReviewRepository.java
+++ src/main/java/com/bookreview/repository/ReviewRepository.java
@@ -1,6 +1,5 @@
 package com.bookreview.repository;
 
-import com.bookreview.entity.Book;
 import com.bookreview.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +7,6 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    List<Review> findByBook(Book book);
+    List<Review> getByBookId(Long bookId);
 
 }
